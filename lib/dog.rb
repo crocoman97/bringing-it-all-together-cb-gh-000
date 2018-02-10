433d330205d7068b680c1e49246edd73529a61a2
Rauof M <abdelrauof197@gmail.com>
2018-02-10 10:12:36 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -45,7 +45,7 @@ class Dog
     sql = <<-SQL
     SELECT * FROM dogs WHERE id = ?
     SQL
-    info = DB[:conn].execute(sql,id).flatten
+    info = DB[:conn].execute(sql,id)
     Song.new(name: info[1], breed: info[2], id: info[0])
   end
   
