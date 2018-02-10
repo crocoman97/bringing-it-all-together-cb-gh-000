d293582ef18e7c4e0b50cb37942b27b417c325b3
Rauof M <abdelrauof197@gmail.com>
2018-02-10 09:38:41 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,7 +9,7 @@ class Dog
   
   def self.create_table
     sql = <<-SQL
-    CREATE TABLE dogs IF NOT EXITS (id INTEGER PRIMARY KEY, name TEXT, breed TEXT)
+    CREATE TABLE IF dogs NOT EXITS (id INTEGER PRIMARY KEY, name TEXT, breed TEXT)
     SQL
     DB[:conn].execute(sql)
   end
